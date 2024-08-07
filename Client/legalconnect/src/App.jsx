100e6a24a87e24789a5704b05c288e716337feea
patilarya76 <patilaryaanil07@gmail.com>
2024-08-07 14:11:12 +0530

    Signinpagedone

--- Client/legalconnect/src/App.jsx
+++ Client/legalconnect/src/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import Landing from "./Pages/Landing";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Landing from './Pages/Landing';
+import Signin from './Pages/Signin';
 
 const App = () => {
   return (
-    <div className="app">
-      <Landing />
-    </div>
+    <Router>
+      <div className="app">
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/signin" element={<Signin />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
---
+++ Client/legalconnect/src/Pages/Signin.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import './Signin.css';
+
+const Signin = () => {
+    return (
+        <div className="signin-container">
+            <div className="navbar">
+                <div className="logo">LegalConnect</div>
+            </div>
+            <div className="signin-box">
+                <h2>Login to LegalConnect</h2>
+                <form>
+                    <div className="form-group">
+                        <label htmlFor="username">Username</label>
+                        <input type="text" id="username" name="username" required />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" id="password" name="password" required />
+                    </div>
+                    <button type="submit" className="login-button">Login</button>
+                </form>
+                <div className="footer-links">
+                    <a href="#">Forgot your password?</a>
+                    <span> | </span>
+                    <a href="#">Create an account</a>
+                </div>
+            </div>
+            <div className="footer">
+                <p>© 2023 LegalConnect. All rights reserved.</p>
+            </div>
+        </div>
+    );
+}
+
+export default Signin;

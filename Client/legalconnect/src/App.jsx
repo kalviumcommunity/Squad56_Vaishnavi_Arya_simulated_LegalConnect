8d7869707b219ea7a70be58e20bfd2a68839d081
patilarya76 <patilaryaanil07@gmail.com>
2024-08-09 14:13:32 +0530

    donewithday10

--- Client/legalconnect/src/App.jsx
+++ Client/legalconnect/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Landing from './Pages/Landing';
 import Signin from './Pages/Signin';
-import Signup from './Pages/Signup'; // Import Signup page
+import Signup from './Pages/Signup'; 
 
 const App = () => {
   return (
@@ -11,7 +11,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/signin" element={<Signin />} />
-          <Route path="/signup" element={<Signup />} /> {/* Add Signup route */}
+          <Route path="/signup" element={<Signup />} />
         </Routes>
       </div>
     </Router>
